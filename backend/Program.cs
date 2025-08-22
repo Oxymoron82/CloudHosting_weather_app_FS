47090772aed3b4cb4e61973047f6008ba8b5170e
Oxymoron82 <oxumoron82@gmail.com>
2025-08-22 12:04:22 +0300

    renewed

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,25 +1,28 @@
+using Microsoft.JSInterop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("hellous/", GetHello);
+app.MapGet("/", () => "Hello Net24S!"); // Using "anonomous functions"
+app.MapGet("hellous/", HelloMehthod);
 
 app.Run();
 
-string GetHello()
-
+string HelloMehthod()
 {
     var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
     var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
 
-    //alternative shorter Syntax
 
-
-
-    //print to console absolute path (FullName)
+    // print to console absolute path (FullName)
     Console.WriteLine($"Reading hello from: {helloPath}");
+    // in spe
 
     var message = File.ReadAllText(helloPath);
-
     return "Read from FILE:\n\n" + message;
+
+
 }
+
+// Deploy with:
+// az webapp up --name maria-cedersten -g test1 --location westeurope --sku B1 --os-type linux
