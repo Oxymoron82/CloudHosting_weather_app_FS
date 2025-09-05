45f588e63ff08739a9a7d1e239d8c081f10e45ca
Oxymoron82 <oxumoron82@gmail.com>
2025-09-05 13:54:41 +0300

    finding problem

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,9 @@
 using Microsoft.JSInterop.Infrastructure;
+Console.WriteLine(@$"Current directory: {Directory.GetCurrentDirectory()}");
+
+Console.WriteLine(@$"Executing Assembly:
+
+{Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}");
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -14,12 +19,23 @@ string GetHello()
 
 //look for all assets in te same folder, where the programm (dll's etc.) ew (usually bin/Debug/net9.0)
 {
-    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+    var helloFolder = new DirectoryInfo(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+
+    //using path.combine allows use to not worry about the operating system separator
+
     var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
 
     //Check that file exists, otherwise it may reult into HTTP ERROR CODE 500
     // print to console absolute path (FullName)
-    Console.WriteLine($"Reading hello from: {helloPath}");
+
+    if (!File.Exists(helloPath))
+    {
+        return
+    @$"Sorry, cant return hello fron file. File '{helloPath}' not found. Plese contact ur support";
+    }
+
+    // file was found OK, print debug info to console absolute path (FullName)
+    Console.WriteLine($"File found! Reading hello from: {helloPath}");
     // in spe
 
     var message = File.ReadAllText(helloPath);
