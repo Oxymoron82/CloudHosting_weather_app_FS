7a32e57387b564da81939a2812f74fe3b0f919c9
Oxymoron82 <oxumoron82@gmail.com>
2025-09-05 12:57:17 +0300

    tehty

--- backend/Program.cs
+++ backend/Program.cs
@@ -11,11 +11,13 @@ app.MapGet("/", () => "Hello Net24S!"); // Using "anonomous functions"
 app.Run();
 
 string GetHello()
+
+//look for all assets in te same folder, where the programm (dll's etc.) ew (usually bin/Debug/net9.0)
 {
     var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
     var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
 
-//Check that file exists, otherwise it may reult into HTTP ERROR CODE 500
+    //Check that file exists, otherwise it may reult into HTTP ERROR CODE 500
     // print to console absolute path (FullName)
     Console.WriteLine($"Reading hello from: {helloPath}");
     // in spe
